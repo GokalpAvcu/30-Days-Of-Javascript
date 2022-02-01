0deeabb20bbd766550d3de3e5824370c6e8271e1
gökalp <avcu219@gmail.com>
2022-02-01 17:01:57 +0300

    add js, html

--- introduction.js
+++ introduction.js
@@ -1,2 +1,18 @@
-console.log('bu yazı introduction.js den geldi!')
-console.log("Gokalp Avcu")
+// console.log('bu yazı introduction.js den geldi!')
+
+console.log(typeof 'Prototurk') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+let youtubeChannel = 'prototurk',   // her seferinde her degiskenin basinda let yazmak yerine virgül koyup tab'a basarak degiskenleri belirtebilirim.
+    channelAge = 11,
+    isChannelStillActive = true,
+    secondChannel = null,
+    channelMoney = undefined
+
+const PI = 3.14 // const ile ifade ettigimiz degiskenleri genelde büyük harfle yazmayı tercih ederim
+const GRAVITY = 9.81
+
+console.log(PI, GRAVITY, youtubeChannel, channelAge, isChannelStillActive, secondChannel, channelMoney);
