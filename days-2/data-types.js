a3cb5d3a8e26d89925fc0222bdcab914b815a3e6
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-10 10:17:29 +0300

    add days 2

---
+++ days-2/data-types.js
@@ -0,0 +1,30 @@
+let numOne = "Gokalp"
+let numTwo = "Gokalp"
+console.log(numOne == numTwo)
+
+let lightOff = false;
+let lightOn = true;
+
+let nums = [1, "prototurk", 3]
+
+// nums[0] // 1
+// nums[1] // prototruk
+// nums[2] // 3
+
+nums[1] = 2 // dizinin birinci elemanı prototurk'u 2 olarak degistik ve consolda bize [1,2,3] yazdırdı
+
+console.log(nums)
+
+
+let name = "Gökalp"
+let surName = "Avcu"
+
+//let fullName = name + " " + surName
+//console.log(fullName)
+
+// TEMPLATE LİTERALS (backtick ve dolar isareti kullanılarak yazılır)
+
+let fullName = `${name} ${surName}`
+
+console.log(fullName)
+
