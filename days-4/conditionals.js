e67e0cbe2038e3366273fc932ce63bb59a8ab196
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-11 17:08:19 +0300

    add conditional

--- days-4/conditionals.js
+++ days-4/conditionals.js
@@ -55,3 +55,94 @@ if (weather === 'rainy') {
 } else {
   console.log('No need for rain coat.')
 }
+
+// switch 
+// Switch, if else if else için bir alternatiftir . switch ifadesi, bir switch anahtar sözcüğü ile başlar, ardından bir parantez ve kod bloğu gelir. Kod bloğunun içinde farklı durumlarımız olacak. 
+// Vaka bloğu, switch ifadesi parantezindeki değer vaka değeriyle eşleşirse çalışır.
+// Break ifadesi, koşul yerine getirildikten sonra kod yürütmesinin düşmemesi için yürütmeyi sonlandırmak içindir. 
+// Varsayılan blok, tüm durumlar koşulu karşılamıyorsa çalışır.
+
+/*switch(caseValue){
+    case 1:
+      // code
+      break
+    case 2:
+     // code
+     break
+    case 3:
+     // code
+     break
+    default:
+     // code
+  } */
+
+  let weatherr = 'cloudy'
+  switch (weatherr) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
+
+
+// Switch More Examples
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+  case 'monday':
+    console.log('Today is Monday')
+    break
+  case 'tuesday':
+    console.log('Today is Tuesday')
+    break
+  case 'wednesday':
+    console.log('Today is Wednesday')
+    break
+  case 'thursday':
+    console.log('Today is Thursday')
+    break
+  case 'friday':
+    console.log('Today is Friday')
+    break
+  case 'saturday':
+    console.log('Today is Saturday')
+    break
+  case 'sunday':
+    console.log('Today is Sunday')
+    break
+  default:
+    console.log('It is not a week day.')
+}
+
+//Examples to use conditions in the cases
+
+let nums = prompt('Enter number');
+switch (true) {
+  case nums > 0:
+    console.log('Number is positive');
+    break;
+  case nums == 0:
+    console.log('Numbers is zero');
+    break;
+  case nums < 0:
+    console.log('Number is negative');
+    break;
+  default:
+    console.log('Entered value was not a number');
+}
+
+// Ternary Operators (üçlü koşullar) 
+// eger daha az koşul varsa bu kullanılabilir
+
+let isCloudy = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
