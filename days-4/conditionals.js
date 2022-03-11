41bdabb28d8afd39dc94d2fdfddedfb94ce0c68e
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-11 16:44:04 +0300

    add conditionals

---
+++ days-4/conditionals.js
@@ -0,0 +1,36 @@
+// Conditionals (Koşullar)
+// if
+/*if (KOŞUL) {
+      // kodlarınız çalışmaya başlar
+} */
+
+let isRaining = true
+if (isRaining){
+    console.log("Git şemsiye al!")
+}
+
+//if-else
+let num = 3
+if (num > 5){
+    console.log(`${num} is a greater than 5`)
+}else{
+    console.log(`${num} is less than 5`)
+}
+
+let isSunny = false
+if(isSunny) {
+    console.log("Şemsiye almana gerek yok.")
+}else{
+    console.log("Hava yağışlı, şemsiye alman gerekir.")
+}
+
+let result = prompt("2 + 5 kaç yapar?")
+
+if(result === "7"){
+    console.log("Doğru cevabi verdin!")
+}else {
+    console.log("Hatalı cevap!")
+}
+
+// If Else if Else
+
