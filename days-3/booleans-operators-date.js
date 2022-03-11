435a7b94d7eb128af9ee8e7155d13183f7d4a80f
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-11 10:09:41 +0300

    add days-3

---
+++ days-3/booleans-operators-date.js
@@ -0,0 +1,16 @@
+let a = 4
+let b = 10
+
+console.log( a += b) // a ile b'yi toplar a degerini 14'e eşitler
+
+let c = 10
+let d = 5
+
+console.log( c-=d) // c'den d'yi cikarir ve c'nin yeni degerini 5'e esitler
+
+const PI = 3.14
+const boilingPoint = 100
+
+console.log(`PI sayısının değeri ${PI} suyun kaynama sıcaklığı ${boilingPoint}'dir`)
+
+
