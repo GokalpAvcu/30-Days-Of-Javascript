d129bd966784b23c17fa0b80e1bbf1d2e62da4e6
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-13 13:04:43 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -2,9 +2,19 @@
 
 // Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
 
-const baseValue = prompt("Üçgenin taban uzunluğunun değerini giriniz");
+/*const baseValue = prompt("Üçgenin taban uzunluğunun değerini giriniz");
 const heightValue = prompt("Üçgenin yükseklik değerini giriniz");
 
 const areaValue = (baseValue * heightValue ) / 2;
 
-console.log(`Üçgenin alanı ${areaValue}`);
+console.log(`Üçgenin alanı ${areaValue}`); */
+
+// Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
+
+const number1 = Number(prompt("A kenarının uzunluğunu giriniz:"));
+const number2 = Number(prompt("B kenarının uzunluğunu giriniz:"));
+const number3 = Number(prompt("C kenarınının uzunluğunu giriniz:"));
+
+const perimeterValue = ( number1 + number2 + number3)
+
+console.log(`Üçgenin Çevresi: ${perimeterValue}`);
