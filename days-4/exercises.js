ce1e7c0ea9a0ca2bd7661fd638e43986631492f2
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-14 10:17:19 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -11,10 +11,18 @@ console.log(`Üçgenin alanı ${areaValue}`); */
 
 // Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
 
-const number1 = Number(prompt("A kenarının uzunluğunu giriniz:"));
+/*const number1 = Number(prompt("A kenarının uzunluğunu giriniz:"));
 const number2 = Number(prompt("B kenarının uzunluğunu giriniz:"));
 const number3 = Number(prompt("C kenarınının uzunluğunu giriniz:"));
 
 const perimeterValue = ( number1 + number2 + number3)
 
-console.log(`Üçgenin Çevresi: ${perimeterValue}`);
+console.log(`Üçgenin Çevresi: ${perimeterValue}`);*/
+
+// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+
+const number1 = Number(prompt("Dikdörtgenin uzun kenarının değerini giriniz!"))
+const number2 = Number(prompt("Dikdörtgenin kısa kenarının değerini giriniz!"))
+
+const perimeterValue = (number1 + number2) * 2
+console.log(`Dikdörtgenin alanı: ${perimeterValue}`)
