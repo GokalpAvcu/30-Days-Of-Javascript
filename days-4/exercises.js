81e29b15bc9efc2252acd4eefcfd64d6cf29f542
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-17 16:15:00 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -11,11 +11,15 @@ console.log(`You will have to wait ${18 - age} years`)
 
 // Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 
-const myAge = 23;
+/*const myAge = 23;
 const yourAge =Number(prompt("Yaşınızı giriniz:"))
 
-if (yourAge < 23){
+if (yourAge < myAge){
     console.log(`Benden ${myAge - yourAge} yaş küçüksünüz.`)
-}else{
+}else if(yourAge > myAge){
     console.log(`Benden ${yourAge - myAge} yaş büyüksünüz.`)
-}
+}else{
+console.log("Yaşlarımız eşit.")
+}*/
+
+// If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
