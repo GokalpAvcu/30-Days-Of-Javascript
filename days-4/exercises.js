f9fa0387b03e406fbb64a6fa4ec56507f17e48db
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-17 15:47:30 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -1,5 +1,13 @@
 // DAY 4- EXERCİSES
 
-// 1-Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+// Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
-const age = Number(promt("Enter your age:"))
+/*const age = Number(prompt("Enter your age:"))
+if (age >= 18) {
+    console.log("You are old enough to drive")
+}else{   
+console.log(`You will have to wait ${18 - age} years`)
+} */
+
+// Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+
