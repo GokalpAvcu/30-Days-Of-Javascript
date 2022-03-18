9ded5f5e1699cbc1dd67ac5cdb6e363e8cb9eeee
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-18 10:12:36 +0300

    add loops

--- days-6/loop.js
+++ days-6/loop.js
@@ -2,10 +2,18 @@
 
 // for Loop
 
-for(let i=0; i <= 5; i++){
+/*for(let i=0; i <= 5; i++){
     console.log(i)
 } // 0 1 2 3 4 5
 
 for(let i=5; i >= 0; i--){
     console.log(i)
 } // 5 4 3 2 1 0
+
+for (let i=0; i <= 100; i++){
+    console.log("i", i)
+} */
+
+for(let i=0; i <= 5; i++){
+    console.log(`${i} * ${i} = ${i * i}`) 
+}
