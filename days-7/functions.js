36e936933181247c7d936d523b2d8ed0f9391965
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-21 15:04:45 +0300

    add functions

--- days-7/functions.js
+++ days-7/functions.js
@@ -1,4 +1,36 @@
 function sayHello(){
     console.log("hello!")
 }
-sayHello();
+sayHello(); // burda yazdigim fonksiyonu cagirmazsam icindeki hicbir zaman gözükmez
+
+// let msg = "merhaba dünya" => bu fonksiyonun icerisine ulasamaz cunku global scope iceride tanimli degil
+
+function sayHi(){
+    let msg = "hello world"
+    console.log(msg)
+}
+//****************************************************************************************************************************************************/
+sayHello()
+
+function multiply(num1, num2){ // parametre değerlerini girdik
+    return num1 * num2 // return ile geriye döndürdük
+}
+
+let total = multiply(5,2)
+console.log("Toplam", total)
+
+//********************************************************************************************************************************************************/
+
+function printFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`
+  }
+  console.log(printFullName('Asabeneh', 'Yetayeh'))
+
+  //************************************************************************************************************************************************/
+
+ function sumTwoNumbers(numOne, numTwo) {
+    let sum = numOne + numTwo
+    return sum
+  }
+  
+  console.log(sumTwoNumbers(10, 20))
