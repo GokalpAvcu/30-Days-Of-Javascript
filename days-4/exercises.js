4b90ed0e0e7df57ce0253829c0af158b75b72410
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-21 10:45:08 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -21,5 +21,16 @@ console.log("Yaşlarımız eşit.")
 }
 
 // If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+// using if else / ternary operator.
+
+let a = 4;
+let b = 3;
+if (a>b){
+    console.log("a is greater than b");
+}else{
+    console.log("a is less than b");
+}
+
+
 
 
