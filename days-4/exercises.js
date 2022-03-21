edc752aff0b332a629b2b426a82911b541db59d4
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-21 11:27:02 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -1,15 +1,15 @@
 // DAY 4- EXERCİSES
 
 // Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
-const age = Number(prompt("Enter your age:"))
+/*const age = Number(prompt("Enter your age:"))
 if (age >= 18) {
     console.log("You are old enough to drive")
 }else{   
 console.log(`You will have to wait ${18 - age} years`)
-} 
+} */
 
 // Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
-const myAge = 23;
+/*const myAge = 23;
 const yourAge =Number(prompt("Yaşınızı giriniz:"))
 
 if (yourAge < myAge){
@@ -18,19 +18,20 @@ if (yourAge < myAge){
     console.log(`Benden ${yourAge - myAge} yaş büyüksünüz.`)
 }else{
 console.log("Yaşlarımız eşit.")
-}
+} */
 
 // If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 // using if else 
-let a = 4;
+/*let a = 4;
 let b = 3;
 if (a>b){
     console.log("a is greater than b");
 }else{
     console.log("a is less than b");
-}
+} */
+
 // ternary operator
-(a > b) ? console.log("a is greater than b"): console.log("a is less than b");
+// (a > b) ? console.log("a is greater than b"): console.log("a is less than b");
 
 // Write a code which can give grades to students according to theirs scores
 let score = Number(prompt("enter your score"));
@@ -55,4 +56,29 @@ switch(true){
 
 }
 
-
+//seasons
+let month = prompt("enter month to check seasons");
+switch (month) {
+    case "september":
+    case "october":
+    case "November":
+        console.log("the season is Autumn");
+        break;
+    case "december":
+    case "january":
+    case "febuary":
+        console.log("the season is Winter");
+        break;
+    case "march":
+    case "april":
+    case "may":
+        console.log("the season is Spring");
+        break;
+    case "june":
+    case "july":
+    case "august":
+        console.log("the season is Summer");
+        break;
+    default:
+        console.log("invalid Month")
+}
