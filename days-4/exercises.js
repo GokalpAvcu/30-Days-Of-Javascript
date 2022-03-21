43d08d8d52da2bf5793c38f339035c961e604823
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-21 10:41:03 +0300

    add exercises

--- days-4/exercises.js
+++ days-4/exercises.js
@@ -1,17 +1,15 @@
 // DAY 4- EXERCİSES
 
 // Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
-
-/*const age = Number(prompt("Enter your age:"))
+const age = Number(prompt("Enter your age:"))
 if (age >= 18) {
     console.log("You are old enough to drive")
 }else{   
 console.log(`You will have to wait ${18 - age} years`)
-} */
+} 
 
 // Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
-
-/*const myAge = 23;
+const myAge = 23;
 const yourAge =Number(prompt("Yaşınızı giriniz:"))
 
 if (yourAge < myAge){
@@ -20,7 +18,7 @@ if (yourAge < myAge){
     console.log(`Benden ${yourAge - myAge} yaş büyüksünüz.`)
 }else{
 console.log("Yaşlarımız eşit.")
-}*/
+}
 
 // If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 
