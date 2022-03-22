6a9453f89507cac33a880f0574aa98c319e4aa47
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-22 09:59:14 +0300

    add functions

--- days-7/functions.js
+++ days-7/functions.js
@@ -63,3 +63,11 @@ const areaOfCircle = (radius) => {
     return area;
   }
 console.log(areaOfCircle(10))
+
+//Arguments
+function sumAllNums() {
+ console.log(arguments)
+}
+
+sumAllNums(1, 2, 3, 4)
+// Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
