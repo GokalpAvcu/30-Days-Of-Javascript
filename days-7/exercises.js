55aa3af59b809a2a678231ce839d228563196f4f
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-22 11:59:34 +0300

    add exercises

---
+++ days-7/exercises.js
@@ -0,0 +1,14 @@
+//Declare a function fullName and it print out your full name.
+function fullName(){
+    console.log("Mario Gomez")
+}
+fullName()
+
+//Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+
+function fullName(firstName,lastName){
+    return `${firstName} ${lastName}`;
+}
+console.log(fullName("Gökalp", "Avcu"));
+
+//Declare a function addNumbers and it takes two two parameters and it returns sum.
