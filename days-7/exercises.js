e7f87ab4eaadbc4a84ec214d7923149c5879a27f
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-22 14:24:09 +0300

    add exercises

--- days-7/exercises.js
+++ days-7/exercises.js
@@ -1,8 +1,8 @@
 //Declare a function fullName and it print out your full name.
-function fullName(){
-    console.log("Mario Gomez")
+function fullName1(){
+    console.log("Sergen Yalçın")
 }
-fullName()
+fullName1()
 
 //Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
 
@@ -20,5 +20,17 @@ console.log(addNumbers(2,5))
 
 //An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle
 
+function areaOfRectangle (length, width){
+    let area = length * width
+    return area
+}
+console.log(areaOfRectangle(3,5))
 
+//A perimeter of a rectangle is calculated as follows: perimeter= 2x(length + width). Write a function which calculates perimeterOfRectangle.
+
+function perimeterOfRectangle(length, width){
+    let area = (length + width) * 2
+    return area
+}
+console.log(perimeterOfRectangle(10,12))
 
