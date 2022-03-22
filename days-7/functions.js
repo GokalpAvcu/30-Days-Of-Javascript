a62ec97b2b332f073ed207fb7a46872e84f35e19
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-22 10:04:42 +0300

    add functions

--- days-7/functions.js
+++ days-7/functions.js
@@ -71,3 +71,16 @@ function sumAllNums() {
 
 sumAllNums(1, 2, 3, 4)
 // Arguments(4) [1, 2, 3, 4, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
+// function declaration
+function sumAllNums() {
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(sumAllNums(1, 2, 3, 4)) // 10
+console.log(sumAllNums(10, 20, 13, 40, 10))  // 93
+console.log(sumAllNums(15, 20, 30, 25, 10, 33, 40))  // 173
