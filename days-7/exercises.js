bb1d58915ce81c565284ea2537fe1161c8f28c9c
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-03-22 12:13:43 +0300

    add exercises

--- days-7/exercises.js
+++ days-7/exercises.js
@@ -14,6 +14,6 @@ console.log(fullName("Gökalp", "Avcu"));
 //Declare a function addNumbers and it takes two two parameters and it returns sum.
 
 function addNumbers(x,y){
-  return x + y;
+    return x + y;
 }
 console.log(addNumbers(2,5))
