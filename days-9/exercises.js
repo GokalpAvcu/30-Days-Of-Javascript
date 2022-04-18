5d7b47dc32b930f06b4b41b2f36ba22db82220e6
DESKTOP-NPTOF76\Gökalp AVCU <avcu219@gmail.com>
2022-04-18 15:13:43 +0300

    add exercises

--- days-9/exercises.js
+++ days-9/exercises.js
@@ -15,17 +15,55 @@ const products = [
   { product: "tea", price: "" },
 ];
 
-//Use forEach to console.log each country in the countries array.
+//3-Use forEach to console.log each country in the countries array.
 countries.forEach((country) => {
   console.log(country);
 });
 
-//Use forEach to console.log each name in the names array.
+//4-Use forEach to console.log each name in the names array.
 names.forEach((name) => {
   console.log(name);
 });
 
-//Use forEach to console.log each number in the numbers array.
+//5-Use forEach to console.log each number in the numbers array.
 numbers.forEach((number) => {
-    console.log(number)
+  console.log(number);
 });
+
+//6-Use map to create a new array by changing each country to uppercase in the countries array. (BÜYÜK HARF)
+let upperCaseCountries = countries.map((country) => {
+  return country.toUpperCase();
+});
+console.log(upperCaseCountries);
+
+//7-Use map to create an array of countries length from countries array.
+let countriesLength = countries.map((country) => {
+  return country.length;
+});
+console.log(countriesLength);
+
+//8-Use map to create a new array by changing each number to square in the numbers array
+let squareNum = numbers.map((number) => {
+  return number * number;
+});
+console.log(squareNum);
+
+//9-Use map to change to each name to uppercase in the names array
+let upperCaseNames = names.map((name) => {
+  return name.toUpperCase();
+});
+console.log(upperCaseNames);
+
+//10-Use map to map the products array to its corresponding prices.
+let prices = products.map((product) => {
+  return `${product.product}: ${product.price}`;
+});
+console.log(prices);
+
+//11-Use filter to filter out countries containing land.
+const countriesWithLand = countries.filter((country) => {
+  return country.toLowerCase().includes("land");
+});
+console.log(countriesWithLand);
+
+//12-Use filter to filter out countries having six character.
